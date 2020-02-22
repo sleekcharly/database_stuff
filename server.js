35f4a9de96845f4c628879ae47cf77506e980062
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-22 17:42:07 +0100

    Second commit

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
 
+const url = 'mongodb://localhost:27017';
+
+const cars = [
+    {model: "chevy", year:2017},
+    {model: "Nissan", year:2000},
+]
 
 app.get('/api/users',(req,res)=>{
-    res.json([
-        { 
-            id:1,
-            username:'Francis'
-        },
-        { 
-            id:2,
-            username:'Jones'
-        }
-    ])
+    MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+        client.db('Cars').collection('items').find().limit(1).skip(1).toArray((err, docs) => {
+            console.log(docs)
+        })   
+    })
 })
 
+// app.get('/api/users',(req,res)=>{
+//     MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+//         client.db('Cars').collection('items').insertMany(cars, (err, res) => {
+//             if(err) {
+//                 return console.log(`ERROR: ${err}`)
+//             }
+//             console.log(res);
+//         })
+        
+//     })
+// })
+
+// app.get('/api/users',(req,res)=>{
+//     MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+//         client.db('Cars').collection('items').insertOne({
+//             model:"Ford",
+//             year:2017
+//         }, (err, res)=>{
+//             if(err) {
+//                 return console.log(`ERROR: ${err}`);
+//             }
+//             console.log(res.ops[0]._id.getTimestamp());
+//         });
+//     })
+// })
+ 
 
 const port = process.env.PORT || 3001;
 
